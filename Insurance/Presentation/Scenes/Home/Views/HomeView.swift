ce0a1ae9eeb6a4109013884183997f16691144fc
Raúl Pera Pairó <raul_pairo@icloud.com>
2021-03-14 22:06:08 +0100

    Feature/clean project structure (#2)
    * Structured project
    
    * gitignore added
    
    * SwiftLint added

--- Insurance/Presentation/Scenes/Home/Views/HomeView.swift
+++ Insurance/Presentation/Scenes/Home/Views/HomeView.swift
@@ -7,15 +7,9 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
