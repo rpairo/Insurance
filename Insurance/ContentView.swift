b4a6d3a26dee6877faa373cd50dbf4627e2aa177
Raúl Pera Pairó <raul_pairo@icloud.com>
2021-03-14 18:11:16 +0100

    Initial Commit

---
+++ Insurance/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Insurance
+//
+//  Created by Raúl Pera Pairó on 14/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Insurance/InsuranceApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsuranceApp.swift
+//  Insurance
+//
+//  Created by Raúl Pera Pairó on 14/3/21.
+//
+
+import SwiftUI
+
+@main
+struct InsuranceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
