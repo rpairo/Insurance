e0eed557d0c44c35e4ce0093a810add578302a7a
Raúl Pera Pairó <raul_pairo@icloud.com>
2021-03-15 01:00:06 +0100

    Fetch logic implemented

---
+++ Insurance/Data/DataSources/Cuvva/DataSources/CuvvaDataSource+Fetch.swift
@@ -0,0 +1,52 @@
+//
+//  CuvvaDataSource+Fetch.swift
+//  Insurance
+//
+//  Created by Raúl Pera Pairó on 15/3/21.
+//
+
+import Foundation
+
+extension CuvvaDataSource {
+    // MARK: Properties
+    var fetchUrl: URL? {
+        guard let endpoint = configuration?.endpoint(.ingredients) else { return nil }
+        guard let url = URL(string: endpoint) else { return nil }
+        return url
+    }
+
+    // MARK: Functionality
+    func fetch() {
+        let session = URLSession(configuration: .default)
+        guard let url = fetchUrl else {
+            // Error
+            return
+        }
+
+        session.dataTask(with: url) { data, _, error in
+            if let error = error {
+                // Error
+            }
+
+            guard let policies = self.decodePolicies(data) else {
+                // Error
+                return
+            }
+
+            guard !policies.isEmpty else {
+                // Error
+                return
+            }
+
+            // Succesful
+        }
+        .resume()
+    }
+
+    func decodePolicies(_ data: Data?) -> [PolicyDTO]? {
+        guard let data = data else { return nil }
+        let decoder = JSONDecoder()
+        guard let recipes = try? decoder.decode([PolicyDTO].self, from: data) else { return nil }
+        return recipes
+    }
+}
--- Insurance/Data/DataSources/Cuvva/DataSources/CuvvaDataSource.swift
+++ Insurance/Data/DataSources/Cuvva/DataSources/CuvvaDataSource.swift
@@ -8,8 +8,16 @@
 import Foundation
 
 struct CuvvaDataSource: PoliciesDataSourceable {
-    // MARK: Functionality
-    func fetch() {
-        // TODO: Implement fetch
+    // MARK: Properties
+    var configuration: CuvvaConfiguration? {
+        let file = "RiM"
+        let type = "plist"
+
+        guard let path = Bundle.main.url(forResource: file, withExtension: type) else { return nil }
+        guard let data = try? Data(contentsOf: path) else { return nil }
+
+        let decoder = PropertyListDecoder()
+        guard let config = try? decoder.decode(CuvvaConfiguration.self, from: data) else { return nil }
+        return config
     }
 }
