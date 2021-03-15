5bc5d476bf2480391f05bfac43c5759e5c7516bf
Raúl Pera Pairó <raul_pairo@icloud.com>
2021-03-15 01:55:24 +0100

    Transformation dto to do implemented

--- Insurance/Data/DataSources/Cuvva/Entities/PolicyDTO.swift
+++ Insurance/Data/DataSources/Cuvva/Entities/PolicyDTO.swift
@@ -15,6 +15,11 @@ struct PolicyDTO {
     var startDate: String
     var endDate: String
     var vehicle: VehicleDTO
+    
+    // MARK: Functionality
+    func transform() -> Policy {
+        Policy()
+    }
 }
 
 extension PolicyDTO: Decodable {
@@ -44,5 +49,11 @@ extension PolicyDTO: Decodable {
     }
 }
 
+extension Array where Element == PolicyDTO {
+    // MARK: Functionality
+    func transform() -> [Policy] {
+        self.map { policy in
+            Policy()
+        }
     }
 }
