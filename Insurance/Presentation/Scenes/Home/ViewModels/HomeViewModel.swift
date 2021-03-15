23a518805760a1bbfaac24c585e5c5a0d1fd8f09
Raúl Pera Pairó <raul_pairo@icloud.com>
2021-03-15 03:26:43 +0100

    HomeViewModel connected (#8)
    * HomeViewModel connected
    
    * Sintactic fixes

---
+++ Insurance/Presentation/Scenes/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewModel.swift
+//  Insurance
+//
+//  Created by Raúl Pera Pairó on 15/3/21.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    // MARK: Lifecycle
+    func onAppear() {
+
+    }
+}
