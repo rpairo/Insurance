163d902d01b3ac38d91a3f636660d9d6e378529d
Raúl Pera Pairó <raul_pairo@icloud.com>
2021-03-20 17:43:02 +0100

    Finished first screen

---
+++ Insurance/Presentation/Scenes/Home/Views/Components/ActivePolicyView.swift
@@ -0,0 +1,45 @@
+//
+//  ActivePolicyView.swift
+//  Insurance
+//
+//  Created by Raúl Pera Pairó on 19/3/21.
+//
+
+import SwiftUI
+
+struct ActivePolicyView: View {
+    // MARK: Properties
+    var report: VehicleReport
+
+    // MARK: View
+    var body: some View {
+        VStack {
+            topSection
+            middleSection
+            bottomSection
+        }
+        .background(Color.white)
+        .cornerRadius(20)
+        .overlay(
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(Color.separator, lineWidth: 1)
+        )
+    }
+
+    var topSection: some View {
+        ReportTopSectionView(report: report)
+            .padding(.bottom, 1)
+            .padding([.horizontal, .top])
+    }
+
+    var middleSection: some View {
+        ReportMiddleSectionView(report: report)
+            .padding(.horizontal)
+    }
+
+    var bottomSection: some View {
+        ReportBottomSectionView(report: report)
+            .padding(.top, 1)
+            .padding([.horizontal, .bottom])
+    }
+}
