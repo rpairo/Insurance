2ac316f5b244d248cfbe3d9c9a07401c6318928a
Raúl Pera Pairó <raul_pairo@icloud.com>
2021-03-21 22:22:43 +0100

    Background and first card createds

--- Insurance/Presentation/Scenes/ActivePolicyDetail/Views/Screens/ActivePolicyDetailView.swift
+++ Insurance/Presentation/Scenes/ActivePolicyDetail/Views/Screens/ActivePolicyDetailView.swift
@@ -13,56 +13,83 @@ struct ActivePolicyDetailView: View {
 
     // MARK: Views
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack {
             VStack(alignment: .leading) {
-                Text("Volkswagen Polo")
-                    .foregroundColor(.textWhite)
-                    .font(.system(size: 15))
-                    .fontWeight(.medium)
+                VStack(alignment: .leading) {
+                    Text("Volkswagen Polo")
+                        .foregroundColor(.textWhite)
+                        .font(.system(size: 15))
+                        .fontWeight(.medium)
 
-                Text("MA77 GRO")
-                    .foregroundColor(.textWhite)
+                    Text("MA77 GRO")
+                        .foregroundColor(.textWhite)
+                        .font(.system(size: 22))
+                        .fontWeight(.bold)
+                }
+                .padding([.horizontal, .top])
+
+                VStack(alignment: .leading) {
+                    Text("Total policies")
+                        .foregroundColor(.textWhite)
+                        .font(.system(size: 15))
+                        .fontWeight(.medium)
+
+                    Text("3")
+                        .foregroundColor(.textWhite)
+                        .font(.system(size: 15))
+                        .fontWeight(.medium)
+                }
+                .foregroundColor(.textWhite)
+                .padding(.horizontal)
+                .padding(.top, 10)
+
+                Button(action: {
+
+                }, label: {
+                    Text("Extend cover")
+                        .foregroundColor(.textWhite)
+                        .font(.system(size: 15))
+                        .fontWeight(.medium)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .padding(.vertical, 8)
+                })
+                .background(Color.bgGreen)
+                .clipShape(RoundedRectangle(cornerRadius: 6))
+                .padding([.horizontal, .bottom])
+                .padding(.top, 5)
+            }
+            .background(Color.bgSurfaceHighlight)
+
+            VStack(alignment: .leading) {
+                Text("Active policy")
+                    .foregroundColor(.textNormal)
+                    .font(.system(size: 16))
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .padding()
+
+                Text("Time remaining")
+                    .foregroundColor(.textMinor)
+                    .font(.system(size: 16))
+                    .fontWeight(.bold)
+                    .padding(.horizontal)
+
+                Text("46 mins")
+                    .foregroundColor(.textHighlight)
                     .font(.system(size: 22))
                     .fontWeight(.bold)
+                    .padding(.horizontal)
             }
-            .padding([.horizontal, .top])
+            .background(Color.bgMain)
+            .padding()
 
-            VStack(alignment: .leading) {
-                Text("Total policies")
-                    .foregroundColor(.textWhite)
-                    .font(.system(size: 15))
-                    .fontWeight(.medium)
-
-                Text("3")
-                    .foregroundColor(.textWhite)
-                    .font(.system(size: 15))
-                    .fontWeight(.medium)
-            }
-            .foregroundColor(.textWhite)
-            .padding(.horizontal)
-            .padding(.top, 10)
-
-            Button(action: {
-
-            }, label: {
-                Text("Extend cover")
-                    .foregroundColor(.textWhite)
-                    .font(.system(size: 15))
-                    .fontWeight(.medium)
-                    .frame(maxWidth: .infinity, alignment: .center)
-                    .padding(.vertical, 8)
-            })
-            .background(Color.bgGreen)
-            .clipShape(RoundedRectangle(cornerRadius: 6))
-            .padding([.horizontal, .bottom])
-            .padding(.top, 5)
+            Spacer()
         }
-        .background(Color.bgSurfaceHighlight)
         .onAppear {
 
         }
-
-        Spacer()
+        .background(Color.bgSurface)
+        .ignoresSafeArea(.all, edges: .bottom)
     }
 }
 
